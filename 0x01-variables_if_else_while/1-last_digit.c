bbac7f3a8f899dc80c71c874e0ee013055360fb0
Kelviz <kelvitachy@gmail.com>
2022-09-09 15:01:11 -0700

    modified 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and not 0\n", n, lsn);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lsn);
 	}
 	return (0);
 }
