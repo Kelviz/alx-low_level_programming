3278d13b5f35350f1e8101bca5ddd9e146bc5680
Kelviz <kelvitachy@gmail.com>
2022-09-14 20:07:25 -0700

    modified 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97;   i <= 122; i++)
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
+	
 	}
-	_putchar('\n');
-	return (0);
-}
-
+        _putchar('\n');
+}	
